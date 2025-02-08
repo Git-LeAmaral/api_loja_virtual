d0093df0d692639a3822b3fd3dfe98ea4335e818
Leandro Amaral <lramaral02m@gmail.com>
2025-02-08 10:54:44 -0300

    primeiro commit

---
+++ src/main/java/org/lehdev/api_loja_virtual/ApiLojaVirtualApplication.java
@@ -0,0 +1,13 @@
+package org.lehdev.api_loja_virtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiLojaVirtualApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiLojaVirtualApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/lehdev/api_loja_virtual/ApiLojaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lehdev.api_loja_virtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiLojaVirtualApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
